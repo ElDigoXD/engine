a6a4da4036ca34c7c18e92a6ad826e81d2b797de
jrodrigoma <jrodrigoma97@gmail.com>
2026-08-29 13:03:12 +0200

    Removed local clang path compiler from presets and added platform defines types.

--- Framework/public/Framework/PlatformDefines.h
+++ Framework/public/Framework/PlatformDefines.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#if defined(__clang__) // Clang
+#define TT_CLANG
+#elif defined(__GNUC__)
+#define TT_GCC
+#elif defined(_MSC_VER)
+#define TT_MSVC
+#elif defined(__INTEL_COMPILER)
+#define TT_INTEL
+#endif
+
 #if defined(TT_CLANG) || defined(TT_GCC)
 #define TT_FORCEINLINE __attribute__((always_inline))
 #elif defined(TT_MSVC)
